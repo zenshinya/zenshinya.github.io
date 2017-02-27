aa591629933c7ac312491ab31cf9c6ea4aa8aa1d
zenshinya <xiezhenjia88@gmail.com>
2017-02-28 00:48:48 +0800

    Added button to switch between different languages for localisation

--- nechronica/lib/index.js
+++ nechronica/lib/index.js
@@ -1,3 +1,7 @@
-$( document ).ready(function() {
-    $("[data-localize]").localize("text/labels", { language: "jp" })
+function onLanguageItemChange() {
+    var lang = $("select#language-switcher option:selected").val();
+    $("[data-localize]").localize("text/labels", { language: lang })
+}
+$(document).ready(function() {
+    $("select#language-switcher").change(onLanguageItemChange).change();
 });
