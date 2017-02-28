4d025526a5456fcafa475b79900c79ebe82bfbc8
zenshinya <xiezhenjia88@gmail.com>
2017-02-28 23:27:47 +0800

    Tweaked UI to make it more responsive

--- nechronica/lib/index.js
+++ nechronica/lib/index.js
@@ -7,37 +7,15 @@ $(document).ready(function() {
  * Create language dropdown
  *
  */
-function initialiseLanguageDropdown() {
-	var flags = [
-		'<img class="flag" src="image/us.png" /><span data-localize="MSG_LANGUAGE_ENGLISH">English</span><span class="value">EN</span>',
-		'<img class="flag" src="image/jp.png" /><span data-localize="MSG_LANGUAGE_JAPANESE">Japanese</span><span class="value">JP</span>'
-	];
+function initialiseLanguageDropdown() {	
+	var INITIAL_LANGUAGE = 'en';
+		
+	$("#language-tab").html('<img class="flag" src="image/' + INITIAL_LANGUAGE + '.png" />');
+	$("[data-localize]").localize("text/labels", { language: INITIAL_LANGUAGE })
 	
-	var dropdownHtml = '<dt><a href="#"><span>' + flags[0] + '</span></a></dt><dd><ul>';
-	for(var i = 0; i < flags.length; i++) {
-		dropdownHtml += '<li><a href="#">' + flags[i] + '</a></li>';
-	}
-	dropdownHtml += '</ul></dd>';
-	$(".dropdown").html(dropdownHtml);
-
-	$(".dropdown dt a").click(function() {
-		$(".dropdown dd ul").toggle();
-	});
-				
-	$(".dropdown dd ul li a").click(function() {
-		var text = $(this).html();
-		$(".dropdown dt a span").html(text);
-		$(".dropdown dd ul").hide();
-		$("[data-localize]").localize("text/labels", { language: getSelectedValue("dropdown") })
-	});
-				
-	function getSelectedValue(id) {
-		return $("#" + id).find("dt a span.value").html();
-	}
-
-	$(document).bind('click', function(e) {
-		var $clicked = $(e.target);
-		if (! $clicked.parents().hasClass("dropdown"))
-			$(".dropdown dd ul").hide();
+	$(".dropdown ul li a").click(function() {
+		var imgHtml = $(this).find("img").clone().get(0);
+		$("#language-tab").html(imgHtml);
+		$("[data-localize]").localize("text/labels", { language: imgHtml.name })
 	});
 }
