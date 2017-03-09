929160fc1c7f874aecfd359a8dca71061f4151e7
zenshinya <xiezhenjia88@gmail.com>
2017-03-09 23:49:53 +0800

    Make dice spin when rolling

--- nechronica/lib/index.js
+++ nechronica/lib/index.js
@@ -28,17 +28,32 @@ function rollDice10(diceId, affectedField, additionalNum) {
 	startRollDice(10, diceId, affectedField, additionalNum);
 }
 
+var DIE_ROLL_CD_INCREMENT = 200;
+
 function startRollDice(maxNum, diceId, affectedField, additionalNum) {
 	$('#' + diceId).addClass('dice-display');
-	rollingDice(maxNum, diceId, affectedField, additionalNum, 20);
+	rollingDice(maxNum, diceId, affectedField, additionalNum, DIE_ROLL_CD_INCREMENT);
 }
 
 function rollingDice(maxNum, diceId, affectedField, additionalNum, i) {
 	if(i == 0) {
+		rotateDice(diceId, 0);
 		return;
 	}
 	var randomNo = Math.floor(Math.random() * maxNum) + 1;
 	$('#' + diceId).text(randomNo);
 	$('#' + affectedField).val(randomNo + additionalNum);
-	setTimeout(function() { rollingDice(maxNum, diceId, affectedField, additionalNum, --i); }, 50);
+	
+	rotateDice(diceId, randomNo * 20);
+	
+	var timeOut = (DIE_ROLL_CD_INCREMENT - i * 1.5 * i) + 1; // Roll for 3s
+	
+	setTimeout(function() { rollingDice(maxNum, diceId, affectedField, additionalNum, --i); }, timeOut);
+}
+
+function rotateDice(diceId, degree) {
+   // For webkit browsers: e.g. Chrome
+   $('#' + diceId).css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
+   // For Mozilla browser: e.g. Firefox
+   $('#' + diceId).css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
 }
