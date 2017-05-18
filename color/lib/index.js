8963741f1b3f3dca45eaa4e1d15347376396128a
zenshinya <xiezhenjia88@gmail.com>
2017-05-18 22:07:02 +0800

    Draw out basic UI layout in tiles

--- color/lib/index.js
+++ color/lib/index.js
@@ -2,10 +2,19 @@ $(document).ready(function() {
 	initialize();
 });
 
+var colorScheme = {
+	0: 'black',
+	1: 'red',
+	2: 'green',
+	3: 'blue'
+};
 
 function initialize() {
 	var width = 20;
 	var height = 20;
+	
+	var tileWidth = 30;
+	var tileHeight = 30;
 	
 	var playground = {};
 	
@@ -13,8 +22,24 @@ function initialize() {
 	for (var y = 0; y < height; y++) {
 		for (var x = 0; x < width; x++) {
 			playground[getToken(x, y)] = 0;
+			getPlaygroundDom().append(
+				`<div id=${getToken(x, y)}" class="tile" style="background-color:${colorScheme[0]}; left:${x * tileWidth}px; top:${y * tileHeight}px; width:${tileWidth}px; height:${tileHeight}px;"></div>`
+			);
 		}
 	}
+	
+	// Create sample
+	createSample(playground);
+}
+
+function createSample(playground) {
+}
+
+function drawUI(playground) {
+}
+
+function getPlaygroundDom() {
+	return $('#playground');
 }
 
 function getToken(x, y) {
